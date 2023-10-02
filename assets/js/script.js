e529bd6c013f3bb37d4d60561bb4a38afd6141de
StefFrustagli <>
2023-10-02 20:53:19 +0000

    Start adding some JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// variables for player and computer hands
+let player = document.querySelector('.player-hand'); // store player hand icon
+let computer = document.querySelector('.pc-hand'); // store pc hand icon
+// variables for scores
+let playerScore = document.querySelector('.playerScore');
+let computerScore = document.querySelector('.computerScore');
+
+let options = document.querySelectorAll('.options .buttons'); // all the 3 buttons
+
+// run function for each loop using arrow function
+options.forEach((option) => {
+    option.addEventListener('click', () => {
+        player.classList.add('shakePlayer');
+        computer.classList.add('shakeComputer');
+        //hand to shake when button is clicked 
+
+        //remove classes to make sure
+        setTimeout(() => {
+            computer.classList.remove('shakePlayer');
+            computer.classList.remove('shakeComputer');
+
+        }, 900);
+    });
+});
