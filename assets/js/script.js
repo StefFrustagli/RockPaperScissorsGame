d4498da5c658e82fa0f57d73827cc59dbda906b7
StefFrustagli <>
2023-10-05 12:13:30 +0000

    Update script.js with changes that try to fix the computer's hand issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,113 +1,168 @@
-// Variables for player & computer hands
-let player = document.querySelector('.player-hand'); // store player hand default icon
-let computer = document.querySelector('.pc-hand'); // store pc hand default icon
-// Variables for player hidden hands
-let playerHandRock = document.querySelector('.player-hand-rock');
-let playerHandPaper = document.querySelector('.player-hand-paper');
-let playerHandScissors = document.querySelector('.player-hand-scissors');
-
-// VAriables for computer hidden hands
-let computerHandRock = document.querySelector('.computer-hand-rock');
-let computerHandPaper = document.querySelector('.computer-hand-paper');
-let computerHandScissors = document.querySelector('.computer-hand-scissors');
-
+// Variables for default hands
+let player = document.querySelector(".player-hand"); // store player hand default icon
+let computer = document.querySelector(".pc-hand"); // store pc hand default icon
+// Variables for buttons
+let options = document.querySelectorAll(".options .buttons"); // all the 3 buttons
+let rockButton = document.getElementById("button-rock");
+let paperButton = document.getElementById("button-paper");
+let scissorsButton = document.getElementById("button-scissors");
 // Variables for scores
 let playerScore = 0;
 let computerScore = 0;
-let playerScore_span = document.getElementById('player-score'); // variables with underscore store DOM elements
-let computerScore_span = document.getElementById('computer-score');
-let scoreArea_div = document.querySelector('.score-area');
+let playerScore_span = document.getElementById("player-score"); // variables with underscore store DOM elements
+let computerScore_span = document.getElementById("computer-score");
+let scoreArea_div = document.querySelector(".score-area");
+// Variables for player hidden hands
+let playerHandRock = document.querySelector(".player-hand-rock");
+let playerHandPaper = document.querySelector(".player-hand-paper");
+let playerHandScissors = document.querySelector(".player-hand-scissors");
+// Variables for computer hidden hands
+let computerHandRock = document.querySelector(".computer-hand-rock");
+let computerHandPaper = document.querySelector(".computer-hand-paper");
+let computerHandScissors = document.querySelector(".computer-hand-scissors");
 
-// Variables for buttons/options
-let options = document.querySelectorAll('.options .buttons'); // all the 3 buttons
-let rock_button = document.getElementById('button-rock');
-let paper_button = document.getElementById('button-paper');
-let scissors_button = document.getElementById('button-scissors');
+// Hands shaking function for each loop using arrow function
+options.forEach((option) => {
+    option.addEventListener("click", () => {
+        player.classList.add("shakePlayer");
+        computer.classList.add("shakeComputer");
+        // Remove classes to make hands shake everytime button is clicled
+        setTimeout(() => {
+            player.classList.remove("shakePlayer");
+            computer.classList.remove("shakeComputer");
 
-// game function
-function game(userChoice) {
-    return userChoice;
+            playComputerTurn();
+            // getComputerChoice();
+
+
+            // Function computer random choice
+            function getComputerChoice() {
+                const choice = ["computer-rock", "computer-paper", "computer-scissors"]; // computer's hands IDs
+                let randomNumber = Math.floor(Math.random() * 3); //making sure only 0, 1 or 2 can be selected
+                let computerChoice = choice[randomNumber];
+                console.log(computerChoice);
+                return computerChoice[randomNumber];
+            }
+
+            // Function computer turn
+            function playComputerTurn() {
+                let computerChoice = getComputerChoice();
+
+                let icon = computer.firstElementChild;
+                icon.classList.remove("fa-hand-fist");
+
+                if (computerChoice == "computer-rock") {
+                    icon.classList.add("fa-hand-back-fist");
+                } else if (computerChoice == "computer-paper") {
+                    icon.classList.add("fa-hand");
+                } else {
+                    icon.classList.add("fa-hand-scissors");
+                }
+            }
+
+
+
+        }, 800); // Make movement smoother
+    });
+});
+
+// EVENT LISTENER for each button (when button is clicked, player's hand has to change)
+rockButton.addEventListener("click", function () {
+    let icon = player.firstElementChild;
+
+    icon.classList.remove("fa-hand-fist");
+    removeIcons(icon);
+    icon.classList.add("fa-hand-back-fist");
+
+    /* Computer turn
+    playComputerTurn(); 
+    getComputerChoice(); */
+
+});
+
+paperButton.addEventListener("click", function () {
+    let icon = player.firstElementChild;
+
+    icon.classList.remove("fa-hand-fist");
+    removeIcons(icon);
+
+    icon.classList.add("fa-hand");
+    removeRotation(icon);
+
+    /* Computer turn
+    playComputerTurn(); 
+    getComputerChoice(); */
+});
+
+scissorsButton.addEventListener("click", function () {
+    let icon = player.firstElementChild;
+
+    icon.classList.remove("fa-hand-fist");
+    removeIcons(icon);
+    icon.classList.add("fa-hand-scissors");
+    icon.classList.add("transform");
+
+    removeRotation(icon);
+
+    /* Computer turn
+    playComputerTurn(); 
+    getComputerChoice(); */
+});
+
+// Function to make sure classes don't get added to each others and buttons run correctly
+
+function removeIcons(icon) {
+    if (icon.classList.contains("fa-hand")) {
+        icon.classList.remove("fa-hand");
+    }
+    if (icon.classList.contains("fa-hand-back-fist")) {
+        icon.classList.remove("fa-hand-back-fist");
+    }
+    if (icon.classList.contains("fa-hand-scissors")) {
+        icon.classList.remove("fa-hand-scissors");
+    }
 
 }
 
-// EVENT LISTENER for each button (when button is clicked, player's hand has to change)
+function removeTransform(icon) {
+    if (icon.classList.contains("transform")) {
+        icon.classList.remove("transform");
+    }
 
-rock_button.addEventListener('click', function () {
-    player.classList.replace('player-hand', 'player-hand-rock');
-    player.classList.remove('hide');
-    
-  
-});
+}
 
-paper_button.addEventListener('click', function () {
-    game('paper');
-
-});
-
-scissors_button.addEventListener('click', function () {
-    game('scissors');
-    
-});
+function removeRotation(icon) {
+    if (icon.classList.contains("rotate-90")) {
+        icon.classList.remove("rotate-90");
+    }
+}
 
 
 
 
+/* Function computer random choice
+function getComputerChoice() {
+    const choice = ["button-rock", "button-paper", "button-scissors"];
+    let randomNumber = Math.floor(Math.random() * 3); //making sure it can select 0, 1 or 2 only
+    let computerChoice = choice[randomNumber];
+    return choice[randomNumber];
+} 
 
+// Function computer turn
+function playComputerTurn() {
+    let computerChoice = getComputerChoice();
 
-// Hands shaking frunction for each loop using arrow function
-options.forEach((option) => {
-    option.addEventListener('click', () => {
-        player.classList.add('shakePlayer');
-        computer.classList.add('shakeComputer');
+    let icon = computer.firstElementChild;
+    icon.classList.remove("fa-hand-fist");
 
-        // Remove classes so that hands shake each time the button is clicled - no need to reload the page
-        setTimeout(() => {
-            player.classList.remove('shakePlayer');
-            computer.classList.remove('shakeComputer');
-
-
-            // Make Player's hand to change depending on the button clicked
-            // NEED TO ADD IT
-
-            
-            // Funtion for computer random choice
-            function getComputerChoice() {
-                let choice = ['button-rock', 'button-paper', 'button-scissors'];
-                let randomNumber = Math.floor(Math.random() * 3); //making sure it can select 0, 1 or 2
-                return choices[randomNumber];
-            }
-            
-            
-            
-
-
-            // Variables for Points
-            let pcPoints = parseInt(computerScore.innerHTML);
-            let playerPoints = parseInt(playerScore.innerHTML);
-
-            // Conditions
-            if (options.innerHTML === "Rock") {
-                if (computerChoice === "Paper") // Point goes to computer
-                    computerScore.innerHTML = pcPoints + 1;
-                else if (computerChoice === "Scissors") // Point go to player
-                    playerScore.innerHTML = playerPoints + 1;
-            } else if (options.innerHTML === "Paper") {
-                if (computerChoice === "Stone") // Point goes to player
-                    playerScore.innerHTML = playerPoints + 1;
-                else if (computerChoice === "Scissors") // Point go to computer
-                    computerScore.innerHTML = pcPoints + 1;
-            } else {
-                if (options.innerHTML === "Scissors") {
-                    if (computerChoice === "Stone") // Point goes to computer
-                        computerScore.innerHTML = pcPoints + 1;
-                    else if (computerChoice === "Paper") // Point go to player
-                        playerScore.innerHTML = playerPoints + 1;
-                }
-
-
-            }
-
-        }, 1000); // Make movement smoother
+    if (computerChoice == "rock") {
+        icon.classList.add("fa-hand-back-fist");
+    } else if (computerChoice == "paper") {
+        icon.classList.add("fa-hand");
+    } else {
+        icon.classList.add("fa-hand-scissors");
+    }
+} */
 
 
 
@@ -115,6 +170,4 @@ options.forEach((option) => {
 
 
 
-    });
-});
 
